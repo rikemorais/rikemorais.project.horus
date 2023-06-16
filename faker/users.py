a1ad2ac0ef34451ab46c0ac4323e26777d574d54
Henrique Morais <rikeaju@hotmail.com>
2023-06-16 19:00:21 -0300

    feat: Criar Estrutura de Dados Modelo de Usuários
    Usando o Faker iremos reproduzir um modelo de dados para os testes.

---
+++ faker/users.py
@@ -0,0 +1,83 @@
+import csv
+from faker import Faker
+import random
+from tqdm import tqdm
+
+
+class Registro:
+    def __init__(self, cpf, nome, sobrenome, email, sexo, nacionalidade, idade, escolaridade):
+        self.cpf = cpf
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.email = email
+        self.sexo = sexo
+        self.nacionalidade = nacionalidade
+        self.idade = idade
+        self.escolaridade = escolaridade
+
+
+class GeradorRegistros:
+    def __init__(self):
+        self.fake = Faker('pt_BR')
+
+    def gerar_cpf(self):
+        return self.fake.unique.random_number(digits=11)
+
+    def gerar_nome(self):
+        return self.fake.first_name()
+
+    def gerar_sobrenome(self):
+        return self.fake.last_name()
+
+    def gerar_email(self):
+        return self.fake.email()
+
+    def gerar_sexo(self):
+        return random.choice(['M', 'F'])
+
+    def gerar_nacionalidade(self):
+        return self.fake.country()
+
+    def gerar_idade(self):
+        return random.randint(18, 99)
+
+    def gerar_escolaridade(self):
+        return random.choice(['Ensino Fundamental', 'Ensino Médio', 'Ensino Superior'])
+
+    def gerar_registro(self):
+        cpf = self.gerar_cpf()
+        nome = self.gerar_nome()
+        sobrenome = self.gerar_sobrenome()
+        email = self.gerar_email()
+        sexo = self.gerar_sexo()
+        nacionalidade = self.gerar_nacionalidade()
+        idade = self.gerar_idade()
+        escolaridade = self.gerar_escolaridade()
+
+        return Registro(cpf, nome, sobrenome, email, sexo, nacionalidade, idade, escolaridade)
+
+    def gerar_registros(self, total_registros):
+        registros = []
+        for _ in tqdm(range(total_registros), desc='Progresso'):
+            registro = self.gerar_registro()
+            registros.append(registro)
+
+        return registros
+
+
+gerador = GeradorRegistros()
+
+total_registros = 10000000
+lista_registros = gerador.gerar_registros(total_registros)
+
+caminho_arquivo = 'data/users.csv'
+
+with open(caminho_arquivo, 'w', newline='', encoding='utf-8') as arquivo_csv:
+    writer = csv.writer(arquivo_csv)
+    writer.writerow(['cpf', 'nome', 'sobrenome', 'email', 'sexo', 'nacionalidade', 'idade', 'escolaridade'])
+
+    for registro in tqdm(lista_registros, desc='Gravando registros'):
+        writer.writerow([registro.cpf, registro.nome, registro.sobrenome, registro.email, registro.sexo,
+                         registro.nacionalidade, registro.idade, registro.escolaridade])
+
+print("Arquivo CSV Salvo com Sucesso!")

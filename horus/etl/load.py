c75f3a91bdc6c3424d9c02507bcecd27d81fbeda
Henrique Morais <rikeaju@hotmail.com>
2023-06-29 23:18:12 -0300

    style: :art: Execução do Black
    Primeiro teste usando o Black.

--- horus/etl/load.py
+++ horus/etl/load.py
@@ -1,23 +1,24 @@
-import os 
+import os
 
 from pyspark.sql import SparkSession
 from pymongo import MongoClient
 
 # Conectar ao MongoDB
-client = MongoClient(os.environ['URI_MONGODB'])
-db = client['Testes']
-collection = db['Teste-001']
+client = MongoClient(os.environ["URI_MONGODB"])
+db = client["Testes"]
+collection = db["Teste-001"]
 
 # Criar uma Sessão do Spark
-spark = SparkSession.builder\
-    .master('local')\
-    .config('spark.driver.memory', '4g') \
-    .config('spark.executor.memory', '4g') \
-    .config('spark.driver.maxResultSize', '2g') \
+spark = (
+    SparkSession.builder.master("local")
+    .config("spark.driver.memory", "4g")
+    .config("spark.executor.memory", "4g")
+    .config("spark.driver.maxResultSize", "2g")
     .getOrCreate()
+)
 
 # Ler o arquivo Parquet
-dataframe = spark.read.parquet('../data/users.parquet/*.parquet')
+dataframe = spark.read.parquet("../data/users.parquet/*.parquet")
 
 # Converter o DataFrame para um RDD de dicionários
 rdd = dataframe.rdd.map(lambda row: row.asDict())
